a9614bcae02ee4358ab1df474909aed3b0c0dcc8
nikita0106 <valinnikita@gmail.com>
2022-11-13 15:33:00 +0300

    initial commit

---
+++ src/main/java/com/holodos/fridgemanager/FridgeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.holodos.fridgemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FridgeManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/holodos/fridgemanager/FridgeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holodos.fridgemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
